614e5178ef9c948c0c0bd5cbef20903c71affd0e
jykim3097 <jykim3097@gmail.com>
2018-09-28 22:09:51 +0900

    [추가] 국/시비 지원 java
    1. 국/시비 지원 액션바랑 연결할 java 파일 추가

---
+++ ReadyWomanNav/app/src/main/java/com/readywoman/codef/readywomannav/NationalTomorrowActivity.java
@@ -0,0 +1,73 @@
+package com.readywoman.codef.readywomannav;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class NationalTomorrowActivity extends AppCompatActivity {
+
+    NationalTomorrowActivity_tab1 fragment1;
+    NationalTomorrowActivity_tab2 fragment2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actionbar);
+
+        //1. 툴바 설정
+        Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //2. xml 프래그먼트를 보여주기
+        fragment1 = new NationalTomorrowActivity_tab1();
+        fragment2 = new NationalTomorrowActivity_tab2();
+
+        //프레그먼트를 메니져로 보여줌
+        getSupportFragmentManager().beginTransaction().add(R.id.container,fragment1).commit();
+
+        //3. 탭기능 구성
+        TabLayout tabs=(TabLayout)findViewById(R.id.tabs);
+        tabs.addTab(tabs.newTab().setText("내일배움카드제"));
+        tabs.addTab(tabs.newTab().setText("센터 연락처"));
+
+
+        //탭버튼을 클릭했을 때 프레그먼트 동작
+        tabs.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+                //선택된 탭 번호 반환
+                int position =tab.getPosition();
+
+                Fragment selected = null;
+
+                if(position == 0 ){
+
+                    selected = fragment1;
+
+                }else if(position == 1 ){
+
+                    selected =fragment2;
+
+                }
+
+                //선택된 프레그먼트로 바꿔줌
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, selected).commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+    }
+}
---
+++ ReadyWomanNav/app/src/main/java/com/readywoman/codef/readywomannav/NationalTomorrowActivity_tab2.java
@@ -0,0 +1,22 @@
+package com.readywoman.codef.readywomannav;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class NationalTomorrowActivity_tab2 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        // xml 로 만들어준 프레그먼트를 자바 단에서 만들어줌
+        ViewGroup rootGroup =(ViewGroup)inflater.inflate(R.layout.activity_national_tomorrow_tab2,container,false);
+        return  rootGroup;
+
+    }
+}
